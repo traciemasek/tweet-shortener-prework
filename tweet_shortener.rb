dcd9cebe6101502de8c949896fdb5ffd2996cd15
Tracie Masek <traciemasek@gmail.com>
2019-07-27 19:35:44 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
+require 'pry'
+
+tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
 # Write your code here.
+def dictionary
+  dictionary = {
+    "hello"=>"hi",
+    "to"=>"2",
+    "two"=>"2",
+    "too"=>"2",
+    "for"=>"4",
+    "four"=>"4",
+    "be"=>"b",
+    "you"=>"u",
+    "at"=>"@",
+    "and"=>"&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet_arr = tweet.split(" ")
+  
+  tweet_arr.map! do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else 
+      word
+    end
+  end
+  tweet_arr.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.size <= 140
+    tweet 
+  else 
+    word_substituter(tweet)
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  new_tweet = word_substituter(tweet)
+  if new_tweet.size > 140
+    return "#{new_tweet[0..136]}..."
+  else
+    new_tweet
+  end
+end
+
+
+
+  
